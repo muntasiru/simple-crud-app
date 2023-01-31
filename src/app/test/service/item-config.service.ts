4991d9be4b47be9d67b37b67167c21827ef4ad0c
muntasir <muntasir.u@coppamet.com>
2023-01-31 18:03:57 +0600

    add price discount total

--- src/app/test/service/item-config.service.ts
+++ src/app/test/service/item-config.service.ts
@@ -5,10 +5,15 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class ItemConfigService {
   private statusSource = new BehaviorSubject(''); // set default status
+  private totalPrice = new BehaviorSubject(0); // set default status
   currentStatus = this.statusSource.asObservable();
+  currentTotal = this.totalPrice.asObservable();
   constructor() {}
 
   changeMessage(data: any) {
     this.statusSource.next(data);
+  }
+  changeTotal(data: any) {
+    this.totalPrice.next(data);
   }
 }
